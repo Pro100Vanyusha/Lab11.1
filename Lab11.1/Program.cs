3476185fb06d29b850ba769409a1acc7f88c9788
Іван Музика <i.muzyka@kpnu.km.ua>
2021-12-17 20:26:13 +0200

    Добавьте файлы проекта.

---
+++ Lab11.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11._1
+{
+    class Program
+    {
+        static string Replace(string str, int pos)
+        {
+            string _str = str.Substring(0, pos);
+            for (int i = pos; i < str.Length; i++)
+                _str += str[i] == '0' ? '1' : (str[i] == '1' ? '0' : str[i]);
+            return _str;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Replace("101010101010", 3));
+            Console.ReadKey(true);
+        }
+    }
+}
